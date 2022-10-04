184833ff4b896153f0b286fe3d06e005be1bc9b4
Matthew Roberts <thew.roberts@gmail.com>
2022-10-04 20:32:31 +0000

    Code in preparation for 10/4/22

---
+++ 3) DRYer Driving/DryerDriving.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.dcs15815;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name = "DRYer Driving", group = "Learning")
+public class DryDrivingOpMode extends LinearOpMode {
+	
+	public void runOpMode() {
+
+	}
+}
