0659a2f967cee9dc94b9e92e53db0e2442a315e1
Matthew Roberts <thew.roberts@gmail.com>
2026-09-21 17:56:57 -0400

    first opmode

---
+++ 1) Simple Driving/SimpleDriving.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.dcs15815;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@Autonomous(name = "Simple Driving", group = "Learning")
+public class SimpleDrivingOpMode extends LinearOpMode {
+
+	@Override
+	public void runOpMode() {
+		// Your code goes here
+	   }
+    }
+}
